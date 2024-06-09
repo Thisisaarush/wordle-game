fcc27bac4d500cd9779e38e47441998403b0ea78
Aarush Tanwar <tanwaraarush007@gmail.com>
2024-06-09 17:07:37 +0530

    feat: Implement caching for word of the day API route in a .json file

--- src/app/api/wordlist/route.ts
+++ src/app/api/wordlist/route.ts
@@ -1,20 +1,38 @@
 import { promises as fs } from "fs"
 import path from "path"
 
-let cachedWord: { word: string; date: string } | null = null
+async function writeCachedWord(word: { word: string; date: string }) {
+  const filePath = path.join(process.cwd(), "src/data", "cachedWord.json")
+  await fs.writeFile(filePath, JSON.stringify(word), "utf8")
+}
+
+async function readCachedWord(): Promise<{
+  word: string
+  date: string
+} | null> {
+  const filePath = path.join(process.cwd(), "src/data", "cachedWord.json")
+  try {
+    const data = await fs.readFile(filePath, "utf8")
+    return JSON.parse(data)
+  } catch (error) {
+    return null
+  }
+}
 
 export async function GET(request: Request) {
   try {
     // Get today's date in YYYY-MM-DD format
     const today = new Date().toISOString().split("T")[0]
+    // Read the cached word from the file
+    let cachedWord = await readCachedWord()
 
     // Check if we have a cached word for today
     if (cachedWord && cachedWord.date === today) {
       return Response.json({ word: cachedWord.word })
     }
 
-    // Read the file content
-    const filePath = path.join(process.cwd(), "public", "wordlist.txt")
+    // Read the file content from wordlist.txt
+    const filePath = path.join(process.cwd(), "src/data", "wordlist.txt")
     const data = await fs.readFile(filePath, "utf8")
 
     // Split the content into lines (words)
@@ -29,6 +47,8 @@ export async function GET(request: Request) {
 
     // Cache the word for today
     cachedWord = { word, date: today }
+    // Write the cached word to the file
+    await writeCachedWord(cachedWord)
 
     return Response.json({ word })
   } catch (error) {
